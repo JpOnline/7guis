9bfb77c3e8fda029b6b445293bb2b1aba7e9f58c
João Paulo Soares <JpSoares106@gmail.com>
2020-12-31 09:31:22 -0300

    Adjust circle diameters

--- src/seven_guis/circle_drawer.cljs
+++ src/seven_guis/circle_drawer.cljs
@@ -5,12 +5,13 @@
     [seven-guis.util :as util]))
 
 (defonce component-ref (atom nil))
-(defonce component-state (atom {:circles {}
-                                :selected-circle-id nil
-                                :undo-states []
-                                :redo-states []
-                                }))
-(defonce menu-position (reagent/atom {:x 50 :y 40}))
+(defonce component-state (reagent/atom {:circles {}
+                                        :selected-circle-id nil
+                                        :undo-states []
+                                        :redo-states []}))
+(defonce ui (reagent/atom {:popup {:x 0 :y 0}
+                           :context-menu {:visibility "hidden"}
+                           :diameter-dialog {:visibility "hidden"}}))
 
 (defn draw-circle [ctx {:keys [x y radius]} fill?]
   (.beginPath ctx)
@@ -53,7 +54,7 @@
     (is (= 5 (points-distance [1 1] [4 5])))
     (is (= 13 (points-distance [1 1] [6 13])))))
 
-(defn mouse-event [evt f]
+(defn canvas-mouse-event [evt f]
   (when-let [canvas (some-> ^js @component-ref .-shadow (.querySelector "canvas"))]
     (let [ctx (.getContext canvas "2d")
           bound (.getBoundingClientRect canvas)
@@ -64,7 +65,7 @@
       (f x y))))
 
 (defn on-mouse-move [evt]
-  (mouse-event
+  (canvas-mouse-event
     evt
     (fn [x y]
       (let [distance-from-mouse (fn [{cx :x cy :y}] (points-distance [x y] [cx cy]))
@@ -73,10 +74,46 @@
           (swap! component-state assoc :selected-circle-id id))))))
 
 (defn on-canvas-click [evt]
-  (mouse-event
+  (canvas-mouse-event
     evt
     (fn [x y]
       (swap! component-state create-circle x y 10))) )
+
+(defn open-context-menu [ui-state evt]
+  (.preventDefault evt)
+  (let [container (some-> ^js @component-ref .-shadow (.querySelector "#container"))
+        bound (.getBoundingClientRect container)
+        x (- (.-clientX evt) (.-left bound))
+        y (- (.-clientY evt) (.-top bound))]
+    (-> ui-state
+        (assoc-in [:popup :x] x)
+        (assoc-in [:popup :y] y)
+        (assoc-in [:context-menu :visibility] "visible"))))
+
+(defn dismiss-popup [ui-state evt]
+  (-> ui-state
+      (assoc-in [:context-menu :visibility] "hidden")
+      (assoc-in [:diameter-dialog :visibility] "hidden")))
+
+(defn open-diameter-dialog [ui-state evt]
+  (-> ui-state
+      (assoc-in [:context-menu :visibility] "hidden")
+      (assoc-in [:diameter-dialog :visibility] "visible")))
+
+(defn update-radius [state new-radius]
+   (let [id (:selected-circle-id state)]
+     (update-in state [:circles id] assoc :radius new-radius)))
+
+(defn diameter-dialog []
+  (when (= "visible" (-> @ui :diameter-dialog :visibility))
+    (let [selected-id (:selected-circle-id @component-state)
+          {:keys [x y radius]} (-> @component-state :circles (get selected-id))]
+      [:div#diameter-dialog.popup
+       [:p "Adjust diameter of circle at ("(.toFixed x)", "(.toFixed y)")"]
+       [:input#range
+        {:type "range"
+         :value (-> @component-state :circles (get selected-id) :radius)
+         :onChange #(swap! component-state update-radius (-> % .-target .-value js/parseInt))}]])))
 
 (defn component []
   [:<>
@@ -85,14 +122,40 @@
     #container {
       position: relative;
     }
-    #menu {
+    .dismiss-popup-layer {
+      z-index: 1199;
+      background-color: rgb(0 0 0 / 0.02);
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      visibility: "(if (or (= (-> @ui :context-menu :visibility) "visible")
+                           (= (-> @ui :diameter-dialog :visibility) "visible"))
+                     "visible"
+                     "hidden")";
+    }
+    .popup {
+      z-index: 1200;
       box-shadow: 2px 2px 10px 0px lightgrey;
       padding: 7px;
       width: fit-content;
       background-color: white;
       position: absolute;
-      left: "(+ 200 (:x @menu-position))"px;
-      top: "(+ 200 94)"px;
+      left: "(-> @ui :popup :x)"px;
+      top: "(-> @ui :popup :y)"px;
+    }
+    #context-menu {
+      visibility: "(-> @ui :context-menu :visibility)";
+    }
+    #diameter-dialog {
+      visibility: "(-> @ui :diameter-dialog :visibility)";
+    }
+    #context-menu:hover {
+      background-color: lightgray;
+      cursor: pointer;
+    }
+    #range {
+      width: 100%;
     }
     canvas {
       width: 100%;
@@ -102,10 +165,13 @@
     ")]
    [:div#container
     [:canvas
-     {:onContextMenu #(js/console.log %)
+     {:onContextMenu #(swap! ui open-context-menu %)
       :onMouseMove on-mouse-move
       :onClick on-canvas-click}]
-    [:div#menu "Adjust diameter.."]]])
+    [:div.dismiss-popup-layer {:onClick #(swap! ui dismiss-popup %)
+                               :onContextMenu #(swap! ui open-context-menu %)}]
+    [:div#context-menu.popup {:onClick #(swap! ui open-diameter-dialog %)} "Adjust diameter.."]
+    [diameter-dialog]]])
 
 (defn ^:dev/after-load register-component! []
   (util/define-custom-element! {:element-name "circle-drawer"

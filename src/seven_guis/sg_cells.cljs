d21b3a98641e16494315d5c68c8060bf0b4eb64c
João Paulo Soares <JpSoares106@gmail.com>
2021-01-18 18:27:13 -0300

    Change propagation

--- src/seven_guis/sg_cells.cljs
+++ src/seven_guis/sg_cells.cljs
@@ -1,9 +1,11 @@
 (ns seven-guis.sg-cells
   (:require
 [cljs.test :refer [deftest testing is run-tests]]
+    [clojure.set :as set]
+    [clojure.string :as string]
     [reagent.core :as reagent]
-    [shadow.cljs.bootstrap.browser :as shadow.bootstrap]
-    [seven-guis.util :as util]))
+    [seven-guis.util :as util]
+    [shadow.cljs.bootstrap.browser :as shadow.bootstrap]))
 
 (defonce component-state (reagent/atom {}))
 
@@ -20,20 +22,49 @@
                        (js/console.log result)
                        (callback result)))))
 
-(defn cell [{:keys [column row string]}]
+(defn cell [{:keys [column row string selected? editing? error value]}]
   (js/console.log (str "Updated "column row" with "string))
   (let [formula? #(when % (clojure.string/starts-with? % "="))
-        error (reagent/atom "") 
-        value (reagent/atom "")]
-    (if (formula? string)
-      (evaluate (subs string 1) #(if (:error %)
-                                   (do 
-                                     (reset! value "#Error")
-                                     (reset! error ^js (.-message ^js (.-cause (:error %)))))
-                                   (reset! value (:value %))))
-      (reset! value string))
-    [:span (when @error {:title @error}) @value])
-  )
+        onInputBlur #(-> %1
+                         (update :ui dissoc :editing)
+                         (assoc-in [:domain (keyword column) row :string] %2)
+                         (assoc-in [:domain (keyword column) row :value] "Loading..."))
+        set-error #(-> %1
+                       (assoc-in [:domain (keyword column) row :value] "#Error")
+                       (assoc-in [:domain (keyword column) row :error] %2))
+        set-value #(-> %1
+                       (assoc-in [:domain (keyword column) row :value] %2)
+                       (update-in [:domain (keyword column) row] dissoc :error))
+        parse-formula (fn [f]
+                        (string/replace f
+                                        #"[a-zA-Z]\d{1,2}"
+                                        #(get-in @component-state [:domain
+                                                                   (-> % first string/upper-case keyword)
+                                                                   (-> % (subs 1) js/parseInt) :value] "")))
+                                        #_(str "(reagent.core/track #(get-in @seven-guis.sg-cells/component-state
+                                                       [:domain
+                                                        "(-> % first string/upper-case keyword)"
+                                                        "(-> % (subs 1) js/parseInt)" :value] \"\"))")
+        process-string (fn [s]
+                         (if (formula? s)
+                           (evaluate (parse-formula (subs s 1)) #(if (:error %)
+                                                   (swap! component-state set-error ^js (.-message ^js (.-cause (:error %))))
+                                                   (swap! component-state set-value (:value %))))
+                           (swap! component-state set-value s)))]
+    (process-string string)
+    #_(when (formula? string) (parse-formula (subs string 1)))
+    (if editing?
+      [:input
+       {:onBlur #(do (swap! component-state onInputBlur (-> % .-target .-value))
+                     #_(process-string (-> % .-target .-value)))
+        :autoFocus true
+        :defaultValue string}]
+      [:span
+       {:class (when selected? "selected")
+        :title (when error error)
+        :onClick #(swap! component-state assoc-in [:ui :selected] #{[column row]})
+        :onDoubleClick #(swap! component-state assoc-in [:ui :editing] [column row])}
+       value])))
 
 (def num-columns 5)
 (def num-rows 7)
@@ -42,12 +73,16 @@
   [:style
    (str "
    .container {
-     display: grid;  
+     display: grid;
      grid: auto-flow / repeat("num-columns", 100px);
    }
    span {
      padding: 1px 5px;
-     border: 1px solid;
+     min-height: 22px;
+     border: 1px solid lightgray;
+   }
+   span.selected {
+     border: 1px solid blue;
    }
    ")])
 
@@ -56,21 +91,27 @@
    [component-style]
    [:div.container
     (doall
-     (for [row (range 1 (inc num-rows))
+     (for [row (range num-rows)
            col (map char (range 65 (+ 65 num-columns)))
            ]
        ^{:key (str col row)}
        [:<>
-        [cell {:column col :row row :string (str col row) #_(get-in @component-state [(keyword col) row])}]
-        #_[:input {:onChange #(swap! component-state assoc-in [(keyword col) row] (-> % .-target .-value))}]
-        #_[:br]]
+        [cell {:column col :row row
+               :selected? ((get-in @component-state [:ui :selected] #{}) [col row])
+               :editing? (= (get-in @component-state [:ui :editing] []) [col row])
+               :string #_(str col row) (get-in @component-state [:domain (keyword col) row :string])
+               :value (get-in @component-state [:domain (keyword col) row :value])
+               :error (get-in @component-state [:domain (keyword col) row :error])}]
+        #_[:input {:onChange #(swap! component-state assoc-in [:domain (keyword col) row] (-> % .-target .-value))}]
+        ]
        ))]
-   [cell {:column "A" :row "1" :value (get-in @component-state [:A 1])}]
-   [cell {:column "B" :row "1" :value (get-in @component-state [:B 1])}]
-   [:input {:onChange #(swap! component-state assoc-in [:A 1] (-> % .-target .-value))}]
+   [cell {:column "A" :row "1" :value (get-in @component-state [:domain :A 1])}]
+   [cell {:column "B" :row "1" :value (get-in @component-state [:domain :B 1])}]
+   [:input {:onChange #(swap! component-state assoc-in [:domain :A 0 :value] (-> % .-target .-value))}]
    [:br]
-   [:input {:onChange #(swap! component-state assoc-in [:B 1] (-> % .-target .-value))}]
+   [:input {:onChange #(swap! component-state assoc-in [:domain :B 1] (-> % .-target .-value))}]
    ;; [:input {:onChange #(reset! b1 (-> % .-target .-value))}]
+   [:br] [:p "Selected " (str (get-in @component-state [:ui :selected]))]
    ]
   )
 

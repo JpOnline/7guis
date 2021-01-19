cab414bf2a4e7aee7aca64573298c288b6cd4483
João Paulo Soares <JpSoares106@gmail.com>
2021-01-19 17:26:50 -0300

    Cell pub/sub for change propagation

--- src/seven_guis/sg_cells.cljs
+++ src/seven_guis/sg_cells.cljs
@@ -11,6 +11,10 @@
 
 (defonce compile-eval-state (cljs.js/empty-state))
 
+(def num-columns 5)
+(def num-rows 7)
+(def cell-regex #"[a-zA-Z]\d{1,2}")
+
 (defn evaluate [s callback]
   (cljs.js/eval-str compile-eval-state
         s
@@ -22,41 +26,72 @@
                        (js/console.log result)
                        (callback result)))))
 
+(defn evaluate-cell! [[column row]]
+  (let [formula? #(when % (clojure.string/starts-with? % "="))
+        parse-formula (fn [formula]
+                        (-> formula
+                            (subs 1)
+                            (string/replace cell-regex
+                                            #(get-in @component-state [:domain
+                                                                       (-> % first string/upper-case keyword)
+                                                                       (-> % (subs 1) js/parseInt) :value] ""))))
+        string (get-in @component-state [:domain column row :string])
+        dependants (get-in @component-state [:domain column row :dependants])
+        set-error #(-> %1
+                       (assoc-in [:domain column row :value] "#Error")
+                       (assoc-in [:domain column row :error] %2))
+        set-value #(-> %1
+                       (assoc-in [:domain column row :value] %2)
+                       (update-in [:domain column row] dissoc :error))
+        set-and-propagate (fn [{:keys [value error]}]
+                            (if error
+                              (swap! component-state set-error ^js (.-message ^js (.-cause error)))
+                              (swap! component-state set-value value))
+                            (dorun (map evaluate-cell! dependants)))]
+    (if (formula? string)
+      (evaluate (parse-formula string) set-and-propagate)
+      (set-and-propagate {:value string}))))
+
+(defn cell-references [formula]
+  (re-seq cell-regex formula)
+  )
+
+(defn unsubscribe [state [dependant-column dependant-row]]
+  (let [old-formula (get-in state [:domain dependant-column dependant-row :string] "")
+        cell-references (re-seq cell-regex old-formula)
+        referenced-column #(-> % first string/upper-case keyword)
+        referenced-row #(-> % (subs 1) js/parseInt)]
+    (reduce #(update-in %1
+                        [:domain (referenced-column %2) (referenced-row %2) :dependants]
+                        (partial remove #{[dependant-column dependant-row]}))
+            state
+            cell-references)))
+
+(defn subscribe [state [dependant-column dependant-row]]
+  (let [new-formula (get-in state [:domain dependant-column dependant-row :string] "")
+        cell-references (re-seq cell-regex new-formula)
+        referenced-column #(-> % first string/upper-case keyword)
+        referenced-row #(-> % (subs 1) js/parseInt)]
+    (reduce #(update-in %1
+                        [:domain (referenced-column %2) (referenced-row %2) :dependants]
+                        conj
+                        [dependant-column dependant-row])
+            state
+            cell-references)))
+
 (defn cell [{:keys [column row string selected? editing? error value]}]
   (js/console.log (str "Updated "column row" with "string))
   (let [formula? #(when % (clojure.string/starts-with? % "="))
         onInputBlur #(-> %1
                          (update :ui dissoc :editing)
-                         (assoc-in [:domain (keyword column) row :string] %2)
-                         (assoc-in [:domain (keyword column) row :value] "Loading..."))
-        set-error #(-> %1
-                       (assoc-in [:domain (keyword column) row :value] "#Error")
-                       (assoc-in [:domain (keyword column) row :error] %2))
-        set-value #(-> %1
-                       (assoc-in [:domain (keyword column) row :value] %2)
-                       (update-in [:domain (keyword column) row] dissoc :error))
-        parse-formula (fn [f]
-                        (string/replace f
-                                        #"[a-zA-Z]\d{1,2}"
-                                        #(get-in @component-state [:domain
-                                                                   (-> % first string/upper-case keyword)
-                                                                   (-> % (subs 1) js/parseInt) :value] "")))
-                                        #_(str "(reagent.core/track #(get-in @seven-guis.sg-cells/component-state
-                                                       [:domain
-                                                        "(-> % first string/upper-case keyword)"
-                                                        "(-> % (subs 1) js/parseInt)" :value] \"\"))")
-        process-string (fn [s]
-                         (if (formula? s)
-                           (evaluate (parse-formula (subs s 1)) #(if (:error %)
-                                                   (swap! component-state set-error ^js (.-message ^js (.-cause (:error %))))
-                                                   (swap! component-state set-value (:value %))))
-                           (swap! component-state set-value s)))]
-    (process-string string)
-    #_(when (formula? string) (parse-formula (subs string 1)))
+                         (unsubscribe [column row])
+                         (assoc-in [:domain column row :string] %2)
+                         (subscribe [column row])
+                         (assoc-in [:domain column row :value] "Loading..."))]
     (if editing?
       [:input
        {:onBlur #(do (swap! component-state onInputBlur (-> % .-target .-value))
-                     #_(process-string (-> % .-target .-value)))
+                     (evaluate-cell! [column row]))
         :autoFocus true
         :defaultValue string}]
       [:span
@@ -65,9 +100,6 @@
         :onClick #(swap! component-state assoc-in [:ui :selected] #{[column row]})
         :onDoubleClick #(swap! component-state assoc-in [:ui :editing] [column row])}
        value])))
-
-(def num-columns 5)
-(def num-rows 7)
 
 (defn component-style []
   [:style
@@ -92,26 +124,26 @@
    [:div.container
     (doall
      (for [row (range num-rows)
-           col (map char (range 65 (+ 65 num-columns)))
+           col (map (comp keyword char) (range 65 (+ 65 num-columns)))
            ]
        ^{:key (str col row)}
        [:<>
         [cell {:column col :row row
                :selected? ((get-in @component-state [:ui :selected] #{}) [col row])
                :editing? (= (get-in @component-state [:ui :editing] []) [col row])
-               :string #_(str col row) (get-in @component-state [:domain (keyword col) row :string])
-               :value (get-in @component-state [:domain (keyword col) row :value])
-               :error (get-in @component-state [:domain (keyword col) row :error])}]
-        #_[:input {:onChange #(swap! component-state assoc-in [:domain (keyword col) row] (-> % .-target .-value))}]
+               :string #_(str col row) (get-in @component-state [:domain col row :string])
+               :value (get-in @component-state [:domain col row :value])
+               :error (get-in @component-state [:domain col row :error])}]
+        #_[:input {:onChange #(swap! component-state assoc-in [:domain col row] (-> % .-target .-value))}]
         ]
        ))]
-   [cell {:column "A" :row "1" :value (get-in @component-state [:domain :A 1])}]
-   [cell {:column "B" :row "1" :value (get-in @component-state [:domain :B 1])}]
-   [:input {:onChange #(swap! component-state assoc-in [:domain :A 0 :value] (-> % .-target .-value))}]
+   [cell {:column :A :row "1" :value (get-in @component-state [:domain :A 1])}]
+   [cell {:column :B :row "1" :value (get-in @component-state [:domain :B 1])}]
+   [:input {:onChange #(swap! component-state assoc-in [:domain :A 0 :dependants] [[:B 0]]) #_(swap! component-state assoc-in [:domain :A 0 :value] (-> % .-target .-value))}]
    [:br]
-   [:input {:onChange #(swap! component-state assoc-in [:domain :B 1] (-> % .-target .-value))}]
+   #_[:input {:onChange #(swap! component-state assoc-in [:domain :B 1 :value] (str (cell-references (-> % .-target .-value))))}]
    ;; [:input {:onChange #(reset! b1 (-> % .-target .-value))}]
-   [:br] [:p "Selected " (str (get-in @component-state [:ui :selected]))]
+   [:br] [:p "Selected " (str @component-state )]
    ]
   )
 
